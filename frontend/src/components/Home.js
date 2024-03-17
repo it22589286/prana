33f989ce9b2ee4bd0117eb9c35ee630713138685
it22589286 <it22589286@my.sliit.lk>
2024-03-18 02:55:14 +0530

    modified  sign in and  signup

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import Image from "./imagefiles/hompage.jpg"
+import "./home.css"
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>
-        home
-      
+    <div className='hero' style={{backgroundImage:`url(${Image})`}}>
+      <div className='content'>
+      <h1>Your home for Yoga</h1>
+      <p>Find Balance, Strength, and Serenity at Prana Yoga Institute!"</p>
+      <Link to=''>Become a member</Link>
+        
+      </div>
     </div>
   )
 }
