1a4dd2aefea9fdc6cbd146bebc63698fa958da0b
IT22330796 <127980161+IT22330796@users.noreply.github.com>
2024-05-14 16:01:31 +0530

    Update trainingroutes.js

--- pranaitp2/pranaitp/prana/backend/routes/trainingroutes.js
+++ pranaitp2/pranaitp/prana/backend/routes/trainingroutes.js
@@ -4,6 +4,7 @@ const express=require("express")
 const trainingmodel = require("../models/trainingmodels");
 
 const router = express.Router();
+//tttt
 
 
 router.get("/_training",async(req,res)=>{
